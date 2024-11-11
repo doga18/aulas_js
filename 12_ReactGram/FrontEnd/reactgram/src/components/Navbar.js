d5facaf126c67f3b9b90a0aefa702396a12a7051
Douglas I. Pfeiffer S. <douglas17_serafim@hotmail.com>
2024-11-11 19:56:56 -0300

    Create a new style css, used based that's used by teacher, i'm go turn it's i a better style.

--- 12_ReactGram/FrontEnd/reactgram/src/components/Navbar.js
+++ 12_ReactGram/FrontEnd/reactgram/src/components/Navbar.js
@@ -1,19 +1,62 @@
+// Importando o css module
 import style from './Navbar.module.css'
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+// importando o css only
+import './Navbar.css'
 
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+
+// Components
+// Importação de ícones.
+import {BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill, BsFillBookmarkFill } from 'react-icons/bs'
+import { FcAbout } from "react-icons/fc";
+import { CiLogin } from "react-icons/ci";
+import { CiLogout } from "react-icons/ci";
 
 const Navbar = () => {
+
+  // do the logout stuff
+  const handlelogout = () => {    
+    // Logout logic here  
+    localStorage.removeItem('token');
+    localStorage.removeItem('user_id');
+
+  };
+
   return (
-    <div className={style.logo}>
-      <span className={style.title}>ReactGram, powered By dips.</span>
-        <div className={style.links}>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/login">Login</NavLink>
-          <NavLink to="/register">Crie sua conta</NavLink>
-          <NavLink to="/about">Sobre</NavLink>
-        </div>
-    </div>
+    <nav id="nav">
+      <Link to="/" className={style.title}>ReactGram</Link>
+      <form>
+        <BsSearch />
+        <input type="text" placeholder="Pesquisar" />
+      </form>
+      <ul id="nav-links">
+        <NavLink to="/">
+          <BsHouseDoorFill />
+        </NavLink>
+        <NavLink to="/login">
+          <CiLogin />
+        </NavLink>
+        <NavLink to="/register">
+          <BsFillCameraFill />
+        </NavLink>
+        <NavLink to="/about">
+          <FcAbout />
+        </NavLink>
+        <Link onClick={() => handlelogout()} className={style.logout}>
+          <CiLogout />
+        </Link>
+      </ul>
+    </nav>
+    // <div className={style.logo}>
+    //   <span className={style.title}>ReactGram, powered By dips.</span>
+    //     <div className={style.links}>
+    //       <NavLink to="/">Home</NavLink>
+    //       <NavLink to="/login">Login</NavLink>
+    //       <NavLink to="/register">Crie sua conta</NavLink>
+    //       <NavLink to="/about">Sobre</NavLink>
+    //     </div>
+    // </div>
   )
 }
 
